2a071b2e2f9f1f804e5e92584ec9e8409df4578a
Nico Schneider <Nico_Schneider@sluz.ch>
2018-03-21 15:06:37 +0100

    Initial commit

---
+++ Projektarbeit/config.php
@@ -0,0 +1,11 @@
+<?php
+const CONFIG =[
+    "databaseHost" => "localhost",
+    "databaseName" => "m307",
+    "databaseUser" => "root",
+    "databasePassword" => "",
+    "databaseDumpFileName" => "database/db.sql",
+    "mysqlExecutable" => "C:/xampp/mysql/bin/mysql.exe",
+    "mysqlDumpExecutable" => "C:/xampp/mysql/bin/mysqldump.exe",
+    "nameOfContentTable" => "toBeDefined"
+];
